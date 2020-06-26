e6e39b2e5f024a3d00fc3eccf424c8a42444979d
ss9963821 <ss9963821@naver.com>
2020-06-26 11:30:57 +0900

    dkdk

---
+++ src/TEST.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+
+public class TEST {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int[] scores = {99,97,98};
+		Arrays.sort(scores);
+		
+		for(int i=0; i<scores.length; i++) {
+			System.out.println("scores[" + i + "]=" + scores[i]);
+		}
+		System.out.println();
+		
+		String[] names = {"�垮瘚�", "夢翕熱", "梯團熱"};
+		Arrays.parallelSort(names);
+		
+		for(int i=0; i<names.length; i++) {
+			System.out.println("name[" + i +"]=" + names[i]);
+		}
+		System.out.println();
+	}
+
+}
+
